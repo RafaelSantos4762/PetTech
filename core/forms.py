b25c82b5d61bc627fac191d66f3e80da5c922bc4
Diego Pereira Lopes <diegoplopes5@gmail.com>
2019-10-23 19:02:23 -0300

    set required in cliente

--- core/forms.py
+++ core/forms.py
@@ -55,7 +55,7 @@ class ProdutoForm(forms.Form):
 
 
 class PedidoForm(forms.Form):
-    cliente = forms.IntegerField()
+    cliente = forms.IntegerField(required=True)
     pagamento = forms.CharField(max_length=20,required=True)
     vendedor  = forms.CharField(max_length=150,required=True)
     observacao = forms.CharField(max_length=200,required=False) 
